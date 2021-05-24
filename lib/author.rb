7a4930f4d51411e117ae2600f8b180de7c5c9070
Xiaochen Xu <xiaochex@gmail.com>
2021-05-24 00:13:44 -0400

    commit tree 00643d80d355da1861d2a9db8e103a06c421b923

---
+++ lib/author.rb
@@ -0,0 +1,62 @@
+# class Author
+#     attr_accessor :name
+#     @@all = []
+#     def initialize(name)
+#         @name = name
+#         @@all << self
+#     end
+
+#     def self.all
+#         @@all
+#     end
+
+#     def posts
+#         Author.all.select{|p| p.author == self}
+#     end
+
+#     def add_post(post)
+#         post.author == self
+#     end
+
+#     def add_post_by_title(title)
+#         post = Post.new(title)
+#         post.author == self
+#     end
+
+#     def self.post_count
+#         Author.all.count
+#     end
+# end
+
+
+class Author
+    attr_accessor :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def add_post(post)
+      post.author = self
+    end
+  
+    def add_post_by_title(title)
+      post = Post.new(title)
+      post.author = self
+    end
+  
+    def posts
+      Post.all.select {|post| post.author == self}
+    end
+  
+    def self.post_count
+      Post.all.count
+    end
+  end
